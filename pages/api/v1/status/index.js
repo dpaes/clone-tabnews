c6855ed97406cf2f18a7cd3aa5618df11f307d00
Daniel Paes <daniel_3XD@hotmail.com>
2024-08-27 23:19:42 -0300

    erro de autenticacao apesar de mudar no .enve subir postgres depois

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ chave: "valor"})
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum');
+  console.log(result);
+  response.status(200).json({ chave: "sao acima da media"});
 }
 
-export default status
+export default status;
