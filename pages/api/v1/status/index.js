25a1ba05099e403ba9c3c4780895a513635c2bc0
Daniel Paes <daniel_3XD@hotmail.com>
2024-09-19 10:24:18 -0300

    finaliza endpoint /api/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum');
-  console.log(result);
-  response.status(200).json({ chave: "sao acima da media"});
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseConnectionsValue = databaseConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
+  //const used_connections = await database.query("SELECT count(*) FROM pg_stat_activity WHERE datname = current_database();");
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    }
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-})
+
+  const responseBody = await response.json();
+  //console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual('16.0');
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
+  // const used_connections = await database.query("SELECT count(*) FROM pg_stat_activity WHERE datname = current_database();");
+  // expect(responseBody.used_connections).toEqual(used_connections);
+});
