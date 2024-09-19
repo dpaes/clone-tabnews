185afc038f56ebfb5af5ec2558dc556ebed7b1bb
Daniel Paes <daniel_3XD@hotmail.com>
2024-09-19 12:58:18 -0300

    testado neon na prod vercel, inserido ternario p/ diferenciar node_env para definir uso de ssl

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  console.log("Credenciais do postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: (process.env.NODE_ENV === "development")? false : true
   });
   try {
     await client.connect();
